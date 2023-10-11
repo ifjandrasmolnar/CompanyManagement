94b491504fdeabf04f58d723fc4aba938063dbaa
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2023-10-11 16:59:44 +0200

    [ ADD ] Login.css, Login.jsx

---
+++ CompanyManagement/CompanyManagement/ClientApp/src/Management/Login.jsx
@@ -0,0 +1,18 @@
+﻿import './Style/Login.css';
+import React from "react";
+
+export default function Login() {
+    return (
+        <div className="container" >
+            <div className="top"></div>
+            <div className="bottom"></div>
+            <div className="center">
+                <h1>Login</h1>
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <h2>&nbsp;</h2>
+            </div>
+        </div>
+    );
+}
+
