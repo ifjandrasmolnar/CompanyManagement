34e39e8f41376032350f798f3cab5374cf8a64b3
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2023-10-11 11:41:55 +0200

    [ ADD ] Start structure of the project /.gitignore

---
+++ CompanyManagement/CompanyManagementTest/CompanyManagementStabTest.cs
@@ -0,0 +1,15 @@
+namespace CompanyManagementTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
