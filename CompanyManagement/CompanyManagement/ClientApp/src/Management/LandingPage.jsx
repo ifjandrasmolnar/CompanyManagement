e2e2d979e659f0a1853e8401a76c62a13709ebf2
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2023-10-12 22:32:01 +0200

    [ UPDATE ] Chart 2 implemented

--- CompanyManagement/CompanyManagement/ClientApp/src/Management/LandingPage.jsx
+++ CompanyManagement/CompanyManagement/ClientApp/src/Management/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import {Navbar} from "reactstrap";
 import Chart1 from "./Charts/Chart1";
+import Chart2 from "./Charts/Chart2";
 export default function LandingPage(){
     
     
     return(
         <div className="management-main-container">
             <Chart1 />
+            <Chart2 />
         </div>
     );
 };
