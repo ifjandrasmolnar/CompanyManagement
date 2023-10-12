99e2f934849f8ab9f7869360d7df7ccca4329829
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2023-10-12 17:20:03 +0200

    [ ADD ] Landing page created

---
+++ CompanyManagement/CompanyManagement/ClientApp/src/Management/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Navbar} from "reactstrap";
+
+export default function LandingPage(){
+    return(
+        <div className="management-main-container">
+            <span>Landing Page</span>
+        </div>
+    );
+};
