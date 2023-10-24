3fd8de56dbb1f6fbf121495704b4148119abf481
ifjandrasmolnar <ifj.andras.molnar@gmail.com>
2023-10-24 13:56:07 +0200

    [ ADD ] Add UsersContext.cs and set it.

---
+++ CompanyManagement/CompanyManagement/Models/UsersContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyManagement.Models;
+
+public class UsersContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+{
+    public UsersContext (DbContextOptions<UsersContext> options)
+        : base(options)
+    {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // It would be a good idea to move the connection string to user secrets
+        options.UseSqlServer("Server=localhost,1433;Database=CompanyManagement;User Id=sa;Password=yourStrong(!)Password;");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
