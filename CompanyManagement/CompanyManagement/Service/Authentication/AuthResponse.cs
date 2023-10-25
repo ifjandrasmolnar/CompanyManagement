9c3813b17fbe0e480926384acc2a0870b4eb8ea8
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2023-10-25 15:25:04 +0200

    [ UPDATE ] Email deleted

--- CompanyManagement/CompanyManagement/Service/Authentication/AuthResponse.cs
+++ CompanyManagement/CompanyManagement/Service/Authentication/AuthResponse.cs
@@ -1,3 +1,3 @@
 namespace CompanyManagement.Service.Authentication;
 
-public record AuthResponse(string Email, string UserName, string Token);
+public record AuthResponse(string UserName, string Token);
