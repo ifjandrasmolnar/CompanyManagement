e853a46101f96f5f6918e8dcdb4d6734a35dc163
McKay <kg89szeged@gmail.com>
2023-10-26 14:13:29 +0200

    [ ADD ] new repository for Users created

---
+++ CompanyManagement/CompanyManagement/Repositories/UsersRepository.cs
@@ -0,0 +1,36 @@
+﻿using CompanyManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyManagement.Repositories;
+
+public class UsersRepository : IUsersRepository
+{
+    private readonly UsersContext _context;
+
+    public UsersRepository(UsersContext context)
+    {
+        _context = context;
+    }
+
+    public List<IdentityUser> GetAllUsers()
+    {
+        List<IdentityUser> users = _context.Users.ToList();
+
+        return users;
+    }
+
+    public List<IdentityRole> GetAllRoles()
+    {
+        List<IdentityRole> roles = _context.Roles.ToList();
+
+        return roles;
+    }
+
+    public List<IdentityUserRole<string>> GetAllUserRoles()
+    {
+        List<IdentityUserRole<string>> userRoles = _context.UserRoles.ToList();
+
+        return userRoles;
+    }
+}
